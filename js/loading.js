088142ce33a1f40406d1feb7a94cb7029293eee0
ewpoon <33796747+ewpoon@users.noreply.github.com>
2020-12-06 18:26:07 -0500

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,4 @@
+//source: https://www.superhi.com/library/posts/how-to-add-a-simple-loading-screen-to-your-site
+setTimeout(function() {
+    document.querySelector("div.loading").classList.add("hidden")
+}, 2200)
